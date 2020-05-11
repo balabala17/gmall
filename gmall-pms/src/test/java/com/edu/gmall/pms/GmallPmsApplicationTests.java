83905e0bad04ea7d452898ff32b3ea099313a4ee
Qian <2406221675@qq.com>
2020-05-11 18:02:08 +0800

    逆向工程

--- gmall-pms/src/test/java/com/edu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/edu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,20 @@
 package com.edu.gmall.pms;
 
+import com.edu.gmall.pms.entity.Product;
+import com.edu.gmall.pms.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Slf4j
 @SpringBootTest
 class GmallPmsApplicationTests {
-
+    @Autowired
+    ProductService productService;
     @Test
     void contextLoads() {
+        Product product = productService.getById(1);
+        System.out.println(product.getBrandName());
     }
 
 }
